bf136f6eb1754c001b6075ef7946630aea605d2d
user-mimin <worksidar@gmail.com>
2024-07-30 01:48:36 +0700

    Update DummyData.php

--- app/Console/Commands/DummyData.php
+++ app/Console/Commands/DummyData.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class dummyData extends Command
+class DummyData extends Command
 {
     /**
      * The name and signature of the console command.

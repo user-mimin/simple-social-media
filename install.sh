3b80890f73aa7c3ec3d48f384c4f248031601ef3
user-mimin <worksidar@gmail.com>
2024-07-30 11:50:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ npm run dev
 cp .env.example .env
 php artisan key:generate
 
-sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.2/g' .env &&
+sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.20.2/g' .env &&
 sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env &&
 
 php artisan migrate

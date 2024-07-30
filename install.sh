ac8e729d2196eaffb64d96fb32c10b24d0312d7c
user-mimin <worksidar@gmail.com>
2024-07-30 14:22:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ php artisan key:generate
 sed -i 's/DB_HOST=127.0.0.1/DB_HOST=192.168.3.2/g' .env &&
 sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env &&
 
-php artisan migrate
-php artisan db:seed
+php artisan migrate 
+yes | php artisan db:seed $@
